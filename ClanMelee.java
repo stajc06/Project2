07f2b68797290a67a3d66500ed73e0768ae21350
Jose Meono <stjam26@moravian.edu>
2017-04-04 09:58:36 -0400

    Jose adding new files

--- ClanMelee.java
+++ ClanMelee.java
@@ -14,6 +14,11 @@ public class ClanMelee {
     int totalClanCount;
     ClanStats clanStats;
     String[] clanNames;
+    public static final int HIT_POINT_CAP = 1000;
+    // number of action points that do not cost additional iteration damage
+    public static final int FREE_ACTION_POINTS = 10;
+    // number of extra action points which cost a single iteration damage point
+    public static final int ACTION_POINTS_PER_ITERATION_DAMAGE_POINT = 2;
 
     /**
      * Runs one round of interactions until one clan is left, or there is a winner
